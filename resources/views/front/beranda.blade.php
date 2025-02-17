50cecc39cf5522b3cedd1f732aa344b40b21d6c9
Layla Techno <154958698+laylatechno@users.noreply.github.com>
2025-02-17 17:18:22 +0700

    blading

--- resources/views/front/beranda.blade.php
+++ resources/views/front/beranda.blade.php
@@ -106,8 +106,76 @@
     </div>
 
 
-    @include('front.products-category-top')
+ 
+<!-- Top Products -->
+<div class="top-products-area py-3">
+    <div class="container">
+        <div class="section-heading d-flex align-items-center justify-content-between dir-rtl">
+            <h6>Produk Pilihan</h6><a class="btn btn-sm btn-light" href="/product">Lihat Semua<i class="ms-1 ti ti-arrow-right"></i></a>
+        </div>
+        <div class="row g-2">
+            @foreach ($data_products as $p)
+            <!-- Product Card -->
+            <div class="col-6 col-md-4">
+                <div class="card product-card">
+                    <div class="card-body">
 
+                        <a class="d-block" href="{{ route('product.product_detail', $p->slug) }}">
+                            <img class="image-thumbnail lazy-img"
+                                src="https://placehold.co/300x200?text=Loading..."
+                                data-src="/upload/products/{{ $p->image }}"
+                                data-original="/upload/products/{{ $p->image }}"
+                                alt="{{ $p->name }}">
+                        </a>
+
+
+                        </a>
+                        <a class="store-badge" href="{{ url('produk?category=' . $p->category->slug) }}">
+                            {{ $p->category->name ?? 'Tanpa Kategori' }}
+                        </a>
+
+                        <a class="product-title" href="{{ route('product.product_detail', $p->slug) }}">
+                            {{ $p->name }}
+                        </a>
+
+
+
+                        <p class="sale-price-new">
+                            Rp {{ number_format($p->cost_price, 0, ',', '.') }}
+                            @if($p->price_before_discount && $p->price_before_discount > 0)
+                            <br> <span class="old-price">Rp {{ number_format($p->price_before_discount, 0, ',', '.') }}</span>
+                            @endif
+                        </p>
+
+                        <a href="">
+                            <p class="custom-badge">{{ $p->user->user }} </p>
+                        </a>
+
+                        <span class="product-note">
+                            {{ $p->note }}
+                        </span>
+                        <br>
+
+
+
+
+                        <!-- Rating -->
+                        <!-- <div class="product-rating"><i class="ti ti-star-filled"></i><i class="ti ti-star-filled"></i><i class="ti ti-star-filled"></i><i class="ti ti-star-filled"></i><i class="ti ti-star-filled"></i></div> -->
+                        <!-- Add to Cart -->
+                        <br>
+                        <form class="add-to-cart-form" data-product-id="{{ $p->id }}">
+                            @csrf
+                            <button class="btn btn-primary btn-sm" type="button"><i class="ti ti-shopping-cart"></i></button>
+                        </form>
+                    </div>
+                </div>
+            </div>
+            @endforeach
+
+
+        </div>
+    </div>
+</div>
 
 
 
@@ -148,8 +216,45 @@
 
 
 
-    @include('front.blog-home')
+   
+  <!-- Weekly Best Sellers-->
+  <div class="weekly-best-seller-area pt-2">
+      <div class="container">
+          <div class="section-heading d-flex align-items-center justify-content-between dir-rtl">
+              <h6>Informasi Terbaru</h6><a class="btn btn-sm btn-light" href="{{ asset('template/front') }}/shop-list.html">
+                  Lihat Semua<i class="ms-1 ti ti-arrow-right"></i></a>
+          </div>
+          <div class="row g-2">
+              @foreach ($data_blogs as $p)
+              <div class="col-12">
+                  <div class="card horizontal-product-card">
+                      <div class="d-flex align-items-center">
+                          <div class="product-thumbnail-side">
+                              <a class="product-thumbnail d-block" href="{{ route('product.product_detail', $p->slug) }}">
+                                  <img
+                                      class="mb-2 lazy-img"
+                                      src="https://placehold.co/300x200?text=Loading..."
+                                      data-src="/upload/blogs/{{ $p->image }}"
+                                      alt="{{ $p->titile }}">
+                              </a>
+                          </div>
+                          <div class="product-description py-2">
+                              <!-- Product Title --><a class="product-title d-block" href="/">{{ $p->title }}</a>
 
+
+                              <p>
+                              {{ $p->writer }}<span class="ms-1"> | {{ $p->posting_date }} </span>
+                              </p>
+                              <p class="sale-price"><i class="ti ti-tag"></i>Olahraga</p>
+                          </div>
+                      </div>
+                  </div>
+              </div>
+              @endforeach
+
+          </div>
+      </div>
+  </div>
 
 
 
@@ -172,10 +277,210 @@
 
 
 
-    @include('front.products-category-discount')
-    @include('front.products-category-one')
-    @include('front.products-category-two')
-    @include('front.products-category-three')
+  
+    <div class="top-products-area py-3">
+        <div class="container">
+            <div class="section-heading d-flex align-items-center justify-content-between dir-rtl">
+                <h6>Produk Promo</h6><a class="btn btn-sm btn-light" href="/l">Lihat Semua<i class="ms-1 ti ti-arrow-right"></i></a>
+            </div>
+            <div class="row g-2">
+                @foreach ($product_discount as $p)
+                <!-- Product Card -->
+                <div class="col-6 col-md-4">
+                    <div class="card product-card">
+                        <div class="card-body">
+
+                        <a class="d-block" href="{{ route('product.product_detail', $p->slug) }}">
+                            <img class="image-thumbnail lazy-img"
+                                src="https://placehold.co/300x200?text=Loading..."
+                                data-src="/upload/products/{{ $p->image }}"
+                                data-original="/upload/products/{{ $p->image }}"
+                                alt="{{ $p->name }}">
+                        </a>
+                            <a class="product-title" href="/">{{ $p->name }}</a>
+
+                            <p class="sale-price-new">
+                                Rp {{ number_format($p->cost_price, 0, ',', '.') }}
+                                @if($p->price_before_discount && $p->price_before_discount > 0)
+                                <br> <span class="old-price">Rp {{ number_format($p->price_before_discount, 0, ',', '.') }}</span>
+                                @endif
+                            </p>
+
+                            <p class="custom-badge">
+                                {{ $p->category->name ?? 'Tanpa Kategori' }}
+                            </p>
+
+                            <span class="product-note">
+                                {{ $p->note }}
+                            </span>
+                            <br><br>
+                            <form class="add-to-cart-form" data-product-id="{{ $p->id }}">
+                                @csrf
+                                <button class="btn btn-primary btn-sm" type="button"><i class="ti ti-shopping-cart"></i></button>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+                @endforeach
+
+
+            </div>
+        </div>
+    </div>
+
+ 
+    <div class="top-products-area py-3">
+        <div class="container">
+            <div class="section-heading d-flex align-items-center justify-content-between dir-rtl">
+                <h6>Kategori {{ $first_category->name }}</h6><a class="btn btn-sm btn-light" href="/l">Lihat Semua<i class="ms-1 ti ti-arrow-right"></i></a>
+            </div>
+            <div class="row g-2">
+                @foreach ($product_first_category as $p)
+                <!-- Product Card -->
+                <div class="col-6 col-md-4">
+                    <div class="card product-card">
+                        <div class="card-body">
+
+                        <a class="d-block" href="{{ route('product.product_detail', $p->slug) }}">
+                            <img class="image-thumbnail lazy-img"
+                                src="https://placehold.co/300x200?text=Loading..."
+                                data-src="/upload/products/{{ $p->image }}"
+                                data-original="/upload/products/{{ $p->image }}"
+                                alt="{{ $p->name }}">
+                        </a>
+                            <a class="product-title" href="/">{{ $p->name }}</a>
+
+                            <p class="sale-price-new">
+                                Rp {{ number_format($p->cost_price, 0, ',', '.') }}
+                                @if($p->price_before_discount && $p->price_before_discount > 0)
+                                <br> <span class="old-price">Rp {{ number_format($p->price_before_discount, 0, ',', '.') }}</span>
+                                @endif
+                            </p>
+
+                            <p class="custom-badge">
+                                {{ $p->category->name ?? 'Tanpa Kategori' }}
+                            </p>
+
+                            <span class="product-note">
+                                {{ $p->note }}
+                            </span>
+                            <br><br>
+                            <form class="add-to-cart-form" data-product-id="{{ $p->id }}">
+                                @csrf
+                                <button class="btn btn-primary btn-sm" type="button"><i class="ti ti-shopping-cart"></i></button>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+                @endforeach
+
+
+            </div>
+        </div>
+    </div>
+
+     
+    <div class="top-products-area py-3">
+        <div class="container">
+            <div class="section-heading d-flex align-items-center justify-content-between dir-rtl">
+                <h6>Kategori {{ $second_category->name }}</h6><a class="btn btn-sm btn-light" href="/l">Lihat Semua<i class="ms-1 ti ti-arrow-right"></i></a>
+            </div>
+            <div class="row g-2">
+                @foreach ($product_second_category as $p)
+                <!-- Product Card -->
+                <div class="col-6 col-md-4">
+                    <div class="card product-card">
+                        <div class="card-body">
+
+                        <a class="d-block" href="{{ route('product.product_detail', $p->slug) }}">
+                            <img class="image-thumbnail lazy-img"
+                                src="https://placehold.co/300x200?text=Loading..."
+                                data-src="/upload/products/{{ $p->image }}"
+                                data-original="/upload/products/{{ $p->image }}"
+                                alt="{{ $p->name }}">
+                        </a>
+                            <a class="product-title" href="/">{{ $p->name }}</a>
+
+                            <p class="sale-price-new">
+                                Rp {{ number_format($p->cost_price, 0, ',', '.') }}
+                                @if($p->price_before_discount && $p->price_before_discount > 0)
+                                <br> <span class="old-price">Rp {{ number_format($p->price_before_discount, 0, ',', '.') }}</span>
+                                @endif
+                            </p>
+
+                            <p class="custom-badge">
+                                {{ $p->category->name ?? 'Tanpa Kategori' }}
+                            </p>
+
+                            <span class="product-note">
+                                {{ $p->note }}
+                            </span>
+                            <br><br>
+                            <form class="add-to-cart-form" data-product-id="{{ $p->id }}">
+                                @csrf
+                                <button class="btn btn-primary btn-sm" type="button"><i class="ti ti-shopping-cart"></i></button>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+                @endforeach
+
+
+            </div>
+        </div>
+    </div>
+
+ 
+    <div class="top-products-area py-3">
+        <div class="container">
+            <div class="section-heading d-flex align-items-center justify-content-between dir-rtl">
+                <h6>Kategori {{ $third_category->name }}</h6><a class="btn btn-sm btn-light" href="/l">Lihat Semua<i class="ms-1 ti ti-arrow-right"></i></a>
+            </div>
+            <div class="row g-2">
+                @foreach ($product_third_category as $p)
+                <!-- Product Card -->
+                <div class="col-6 col-md-4">
+                    <div class="card product-card">
+                        <div class="card-body">
+
+                        <a class="d-block" href="{{ route('product.product_detail', $p->slug) }}">
+                            <img class="image-thumbnail lazy-img"
+                                src="https://placehold.co/300x200?text=Loading..."
+                                data-src="/upload/products/{{ $p->image }}"
+                                data-original="/upload/products/{{ $p->image }}"
+                                alt="{{ $p->name }}">
+                        </a>
+                            <a class="product-title" href="/">{{ $p->name }}</a>
+
+                            <p class="sale-price-new">
+                                Rp {{ number_format($p->cost_price, 0, ',', '.') }}
+                                @if($p->price_before_discount && $p->price_before_discount > 0)
+                                <br> <span class="old-price">Rp {{ number_format($p->price_before_discount, 0, ',', '.') }}</span>
+                                @endif
+                            </p>
+
+                            <p class="custom-badge">
+                                {{ $p->category->name ?? 'Tanpa Kategori' }}
+                            </p>
+
+                            <span class="product-note">
+                                {{ $p->note }}
+                            </span>
+                            <br><br>
+                            <form class="add-to-cart-form" data-product-id="{{ $p->id }}">
+                                @csrf
+                                <button class="btn btn-primary btn-sm" type="button"><i class="ti ti-shopping-cart"></i></button>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+                @endforeach
+
+
+            </div>
+        </div>
+    </div>
+
 
 
     <script>
